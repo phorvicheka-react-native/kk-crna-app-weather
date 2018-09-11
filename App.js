609e49a1069914026451959cb2e915811a8bb799
Vicheka PHOR <vicheka.phor@allweb.com.kh>
2018-09-11 16:14:07 +0700

    crna-app-02: UI Implementation - add an input field (TextInput) for the user to change locations
    - We can see that the text input has a default underline on Android.
    - clearButtonMode prop, which only available on iOS, shows a button on the right side of the input field when characters are inserted that allows us to clear the text.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View, Platform} from 'react-native';
+import {StyleSheet, Text, View, Platform, TextInput} from 'react-native';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +8,14 @@ export default class App extends React.Component {
         <Text style={[styles.largeText, styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+
+        <TextInput
+          autoCorrect={false}
+          placeholder="Search any city"
+          placeholderTextColor="white"
+          style={styles.textInput}
+          clearButtonMode="always"
+        />
       </View>
     );
   }
@@ -29,5 +37,15 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18
-  }
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
+  },
 });
