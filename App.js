c8bb501604ab542f8df065d846710d99eeda8ab9
Vicheka PHOR <vicheka.phor@allweb.com.kh>
2018-09-11 17:00:48 +0700

    crna-app-04: UI Implementation -replace TextInput with Custom components : SearchInput
    - Remove alginItems property of containter style in App.js so that the child component take up whole width of the parent
    
    - Let’s break down what this file contains:
    • We export a component named SearchInput.
    • This component accepts a placeholder prop.
    • This component returns a React Native TextInput with a few of its properties specified wrapped within a View. We’ve applied the appropriate styles to our view container including a borderRadius. We also added underlineColorAndroid="transparent" to remove the dark underline that shows by default on Android.
    
    - By moving the entire TextInput details into a separate component called SearchInput, we’ve made sure to:
    1/ not have any of its specific implementation details showing in the parent component anymore
    2/ remove the text input’s styling defined within the styles object
    3/ allows us to build a more manageable application that’s easier to control and understand

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {StyleSheet, Text, KeyboardAvoidingView, Platform, TextInput} from 'react-native';
 
+import SearchInput from './components/SearchInput';
+
 export default class App extends React.Component {
   render() {
     return (
@@ -9,13 +11,7 @@ export default class App extends React.Component {
         <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
 
-        <TextInput
-          autoCorrect={false}
-          placeholder="Search any city"
-          placeholderTextColor="white"
-          style={styles.textInput}
-          clearButtonMode="always"
-        />
+        <SearchInput placeholder="Search any city" />
       </KeyboardAvoidingView>
     );
   }
@@ -25,7 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center'
   },
   textStyle: {
@@ -37,15 +32,5 @@ const styles = StyleSheet.create({
   },
   smallText: {
     fontSize: 18
-  },
-  textInput: {
-    backgroundColor: '#666',
-    color: 'white',
-    height: 40,
-    width: 300,
-    marginTop: 20,
-    marginHorizontal: 20,
-    paddingHorizontal: 10,
-    alignSelf: 'center',
   },
 });
