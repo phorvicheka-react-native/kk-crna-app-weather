c80afbc6815e97f5ea786b1aa86430b0a8374faf
Vicheka PHOR <vicheka.phor@allweb.com.kh>
2018-09-12 09:43:14 +0700

    crna-app-09: Functionalities Implementation - updating weather information whenever the user changes or submits the input field
    a. Networking
    - use MetaWeather API to fetch real weather information: https://www.metaweather.com/api/
    MetaWeather is a weather data aggregator that calculates the most likely outcome from predictions of different forecasters. They provide an API that provides this information over a set of different endpoints:
    1. Location search (/api/location/search/) which allows us to search for a particular city
    2. Location weather information (/api/location/{woeid}) which provides a 5 day forecast for a certain location
    3. Location day which provides(/api/location/{woeid}/{date}/)forecast history and information for a particular day and location
    
    WOEID, or Where On Earth ID, is a location identifier that allows us find details about a specific location.
    
    b. API logic Implementation
    - In the utils directory, we’ve set up two separate API calls in api.js:
    • fetchLocationId returns an array of locations based on a search query
    • fetchWeather returns weather details about a specific location using a location identifier known as Where On Earth ID
    
    - We just updated our state object to have properties:
    • loading: represents when a call is still being made(in order to show a loading icon)
    • error: used to store the error message if our call fails or returns unusable information.
    • location, temperature, and weather : are data we’ll retreive from the API
    
    - With setState,updates to our state can happen asynchronously. For this reason,the method accepts a callback as an optional second parameter that allows us to define an action to fire after the state is updated.
    1/ We first check if city variable is not empty
    2/ Then, we call fetchLocationId with the user queried city (if present)
    3/ and pass the locationID to fetchWeather to return an object that contains the required information(location,weather,and temperature).
    4/ Once complete, our state is updated with the correct parameters.
    5/ If any of the calls happen to error, the catch statement will update the error property in our state to true.
    
    c. Updating the UI
    - We’ve added the following imports:
    • ActivityIndicator is a built-in component that displays a circular loading spinner. We’ll use it when data is being fetched from the network
    • StatusBar is a built-in component that allows us to modify the app status bar at the top of the device
    
    - Conditional Rendering
    • show an error message if the user types in an incorrect address or our API call fails
    • render the correct weather information for a certain location after fetching the data or the API call is done fetching
    we don’t want to show any text fields or the search input while the API call is being fetched

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import {StyleSheet, Text, KeyboardAvoidingView, Platform, TextInput, ImageBackground, View} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  ImageBackground,
+  View,
+  ActivityIndicator,
+  StatusBar,
+} from 'react-native';
 
+import { fetchLocationId, fetchWeather } from './utils/api';
 import getImageForWeather from './utils/getImageForWeather';
 
 import SearchInput from './components/SearchInput';
@@ -10,7 +21,11 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
+      error: false,
       location: '',
+      temperature: 0,
+      weather: '',
     };
   }
 
@@ -18,28 +33,74 @@ export default class App extends React.Component {
     this.handleUpdateLocation('San Francisco');
   }
 
-  handleUpdateLocation = city => {
-    this.setState({
-      location: city,
+  handleUpdateLocation = async city => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId,
+        );
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
     });
   };
   
   render() {
-    const { location } = this.state;
+    const { loading, error, location, weather, temperature } = this.state;
 
     return (
       <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
         <ImageBackground
-          source={getImageForWeather('Clear')}
+          source={getImageForWeather(weather)}
           style={styles.imageContainer}
           imageStyle={styles.image}
         >
           <View style={styles.detailsContainer}>
-            <Text style={[styles.largeText, styles.textStyle]}>{location}</Text>
-            <Text style={[styles.smallText, styles.textStyle]}>Light Cloud</Text>
-            <Text style={[styles.largeText, styles.textStyle]}>24°</Text>
+            <ActivityIndicator animating={loading} color="white" size="large" />
+            
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city.
+                  </Text>
+                )}
 
-            <SearchInput placeholder="Search any city" onSubmit={this.handleUpdateLocation}/>
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {`${Math.round(temperature)}°`}
+                    </Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
           </View>
         </ImageBackground>
       </KeyboardAvoidingView>
